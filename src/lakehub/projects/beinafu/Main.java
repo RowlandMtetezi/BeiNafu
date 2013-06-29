401e174c66247e46fe5d146ce4d42029b25d3a7b
Simeon Obwogo <simeon.obwogo79@gmail.com>
2013-06-29 13:46:33 +0300

    Added Images
    Bottom image, divider image, textured image, background image

--- src/lakehub/projects/beinafu/Main.java
+++ src/lakehub/projects/beinafu/Main.java
@@ -3,6 +3,11 @@ package lakehub.projects.beinafu;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.view.View;
 
 public class Main extends Activity {
 
@@ -10,6 +15,20 @@ public class Main extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ListView menuList = (ListView) findViewById(R.id.ListView_Menu);
+        String[] items = {getResources().getString(R.string.electronics),getResources().getString(R.string.houselifestyle),
+        		getResources().getString(R.string.mobilephones),getResources().getString(R.string.mapretails),
+        		getResources().getString(R.string.miscellaneous)};
+        ArrayAdapter<String> adpt = new ArrayAdapter<String>(this, R.layout.menu_item,items);
+        menuList.setAdapter(adpt);
+        
+        menuList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        	public void onItemClick(AdapterView<?> parent, View itemClicked,int position, long id){
+        		TextView textView = (TextView) itemClicked;
+        		String strText = textView.getText().toString();
+        		//if (strText.equalsIgnoreCase(getResources().getString(
+        	}
+		});
     }
 
 
